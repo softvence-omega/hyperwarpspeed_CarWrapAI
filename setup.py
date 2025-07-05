8e0cb15f229e70b4cbce782cc34c91a30d150dae
Solaiman Hossain <slmnsnto@gmail.com>
2025-07-06 00:32:35 +0600

    Create project architecture files, no implementation yet

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='CarWrapper',
+    version='0.1.0',
+    author='Softvence Omega',
+    packages=find_packages(),
+    install_requires=[]
+)
